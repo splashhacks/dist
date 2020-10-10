7e505e42c185d867a113cc0c8a750fa29fb09220
jacobzwang <zwang.jacob@gmail.com>
2020-10-09 19:48:13 -0700

    deploy

---
+++ canvas.js
@@ -0,0 +1,9 @@
+let canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.onresize = function() {
+  
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+};
